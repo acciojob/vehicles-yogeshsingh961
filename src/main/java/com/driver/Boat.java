8bee463c6b517318bfa24cb7c9120114f1de1b07
Yogesh Singh <yogeshsingh3596@gmail.com>
2023-02-13 13:58:10 +0530

    Vehicle with Boat Constructor

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -20,4 +20,20 @@ public class Boat implements WaterVehicle{
     public int getVehicleCapacity() {
         return capacity;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
